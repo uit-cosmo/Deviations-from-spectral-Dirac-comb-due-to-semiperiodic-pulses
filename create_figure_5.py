d3c1659cd03038ce47f0278ca8d334b090f55e89
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-02-17 15:02:06 +0100

    add script for figure 5

---
+++ create_figure_5.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+import cosmoplots
+from support_functions import *
+import model.forcing as frc
+import model.point_model as pm
+import model.pulse_shape as ps
+
+axes_size = cosmoplots.set_rcparams_dynamo(plt.rcParams, num_cols=1, ls="thin")
+
+fig_PSD = plt.figure()
+ax1 = fig_PSD.add_axes(axes_size)
+fig_AC = plt.figure()
+ax2 = fig_AC.add_axes(axes_size)
+
+
+class AsymLaplaceAmp(frc.ForcingGenerator):
+    def __init__(self, control_parameter):
+        self.control_parameter = control_parameter
+
+    def get_forcing(self, times: np.ndarray, gamma: float) -> frc.Forcing:
+        total_pulses = int(max(times) * gamma)
+        arrival_time_indx = (
+            np.arange(start=0, stop=99994, step=5) * 100
+        )  # multiplied with inverse dt
+        amplitudes = sample_asymm_laplace(
+            alpha=0.5
+            / np.sqrt(
+                1.0 - 2.0 * self.control_parameter * (1.0 - self.control_parameter)
+            ),
+            kappa=self.control_parameter,
+            size=total_pulses,
+        )
+        durations = np.ones(shape=total_pulses)
+        return frc.Forcing(
+            total_pulses, times[arrival_time_indx], amplitudes, durations
+        )
+
+    def set_amplitude_distribution(
+        self,
+        amplitude_distribution_function,
+    ):
+        pass
+
+    def set_duration_distribution(self, duration_distribution_function):
+        pass
+
+
+model = pm.PointModel(gamma=0.2, total_duration=100000, dt=0.01)
+model.set_pulse_shape(ps.LorentzShortPulseGenerator(tolerance=1e-5))
+
+for control_parameter in [0.1, 0.2, 0.3, 0.4]:
+    model.set_custom_forcing_generator(
+        AsymLaplaceAmp(control_parameter=control_parameter)
+    )
+
+    T, S = model.make_realization()
+    forcing = model.get_last_used_forcing()
+    amp = forcing.amplitudes
+
+    S_norm = (S - S.mean()) / S.std()
+
+    f, Pxx = signal.welch(x=S_norm, fs=100, nperseg=S.size / 30)
+
+    ax1.semilogy(f, Pxx, label=rf"$\alpha = {control_parameter}$")
+    PSD = PSD_periodic_arrivals(
+        2 * np.pi * f, td=1, gamma=0.2, Arms=amp.std(), Am=np.mean(amp), S=S
+    )
+    tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
+    ax2.plot(tb, R, label=rf"$\alpha = {control_parameter}$")
+
+t, R_an = calculate_R_an(0, 1, 0.2)
+
+PSD = PSD_periodic_arrivals(2 * np.pi * f, td=1, gamma=0.2, Arms=1, Am=0, S=S)
+ax1.semilogy(
+    f, PSD, "--k", label=r"$S_{\widetilde{\Phi}}(f), \, \langle A \rangle = 0$"
+)
+
+ax2.plot(t, R_an, "--k", label=r"$R_{\widetilde{\Phi}}(t), \, \langle A \rangle = 0$")
+
+ax1.set_xlim(-0.2, 12)
+ax1.set_ylim(1e-14, 1e3)
+ax1.set_xlabel(r"$f$")
+ax1.set_ylabel(r"$S_{\widetilde{\Phi}}(f)$")
+
+ax1.legend()
+ax1.set_xlim(-0.03, 1)
+ax1.set_ylim(1e-4, 1e3)
+ax2.set_xlim(0, 50)
+ax2.set_xlabel(r"$t$")
+ax2.set_ylabel(r"$R_{\widetilde{\Phi}}(t)$")
+ax2.legend()
+cosmoplots.change_log_axis_base(ax1, "y", base=10)
+
+fig_PSD.savefig("PSD_asym_lap.eps", bbox_inches="tight")
+fig_AC.savefig("AC_asym_lap.eps", bbox_inches="tight")
+
+plt.show()
