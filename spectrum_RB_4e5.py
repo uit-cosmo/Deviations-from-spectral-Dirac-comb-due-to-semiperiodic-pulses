bd667942ec678d13076079833e7bfe71e81f87f5
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-05-19 14:40:13 +0200

    script for quasiperiodic 4e5 time series

---
+++ spectrum_RB_4e5.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+
+ts = np.load("RB_time_series_4e5.npy")
+
+intervals_start = [600, 3600, 6800, 9400, 12500, 15500, 18400, 21400, 24400, 27400]
+
+dt = 0.01/200
+time = np.linspace(0,dt*len(ts)-dt, num = len(ts))
+
+Pxx_average = [0]*801
+
+for i in range(len(intervals_start)):
+		ts_interval = ts[intervals_start[i]:intervals_start[i]+1600]
+		f, Pxx = signal.welch(ts_interval, 1/dt, nperseg=len(ts_interval)/1)
+		Pxx_average += Pxx
+
+Pxx_average /= 10
+
+plt.figure()
+plt.xlabel('t')
+plt.ylabel('theta')
+plt.plot(time,ts)
+
+plt.figure()
+plt.semilogy(f, Pxx_average)
+plt.xlabel('f')
+plt.ylabel('PSD(f)')
+plt.show()
