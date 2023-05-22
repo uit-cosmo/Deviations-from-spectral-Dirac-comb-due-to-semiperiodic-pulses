1f225fdd12c50772e0b5fc0355c3cb52a01ef5d3
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-05-22 12:47:01 +0200

    shorten time window to quasi-constant part

--- spectrum_RB_2e6.py
+++ spectrum_RB_2e6.py
@@ -4,17 +4,20 @@ from scipy import signal
 
 ts = np.load("RB_time_series_2e6.npy")
 
-intervals_start = [10000, 14800, 18600, 21200, 28000, 30500, 33000, 37800, 40500, 45400]
+intervals_start = [10000, 18600, 21200, 28000, 30500, 33000, 37800, 40500, 45400, 52600]
 
 dt = 0.01/200
 time = np.linspace(0,dt*len(ts)-dt, num = len(ts))
 
-Pxx_average = [0]*601
+Pxx_average = [0]*301
 
 for i in range(len(intervals_start)):
-		ts_interval = ts[intervals_start[i]:intervals_start[i]+1200]
-		f, Pxx = signal.welch(ts_interval, 1/dt, nperseg=len(ts_interval)/1)
-		Pxx_average += Pxx
+    # + 600 in order to get the constant part 
+    ts_interval = ts[intervals_start[i] + 600 :intervals_start[i]+1200]
+    plt.plot(ts_interval)
+    plt.show()
+    f, Pxx = signal.welch(ts_interval, 1/dt, nperseg=len(ts_interval)/1)
+    Pxx_average += Pxx
 
 Pxx_average /= 10
 
