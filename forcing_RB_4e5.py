ab751c9c007b8a6e338bc55570e6bcd5592c5f4a
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-05-24 14:16:10 +0200

    spectrum of forcing addedf

--- forcing_RB_4e5.py
+++ forcing_RB_4e5.py
@@ -8,8 +8,6 @@ axes_size = cosmoplots.set_rcparams_dynamo(plt.rcParams, num_cols=1, ls="thin")
 
 ts = np.load("RB_time_series_4e5.npy")
 
-intervals_start = 6800
-
 ts_interval = ts[6800:8400]
 ts_interval = (ts_interval - np.mean(ts_interval)) / np.std(ts_interval)
 
@@ -17,14 +15,20 @@ dt = 0.01 / 200
 f, Pxx = signal.welch(ts_interval, 1 / dt, nperseg=len(ts_interval) / 1)
 time = np.linspace(0, dt * len(ts_interval) - dt, num=len(ts_interval))
 
-time_series_fit, symbols, duration_time = create_fit_RB(
+time_series_fit, symbols, duration_time, forcing = create_fit_RB(
     "4e5", f, dt, Pxx, ts_interval, time
 )
 
-plt.plot(time, ts_interval)
-plt.text(x=0.055, y=2, s=r"$Ra = 4\times 10^{5}$")
-plt.plot(time, time_series_fit, "--")
-plt.xlabel(r"$t$")
-plt.ylabel(r"$\widetilde{n}$")
-plt.savefig("time_series_4e5.eps", bbox_inches="tight")
-# plt.show()
+amplitudes = forcing[forcing != 0]
+arrival_times = time[forcing != 0]
+duration_times = np.diff(arrival_times)
+
+f_forcing, Pxx_forcing = signal.welch(forcing, 1/dt, nperseg=len(forcing)/1)
+plt.semilogy(f_forcing, Pxx_forcing)
+
+plt.xlabel(r"$f$")
+plt.ylabel(r"$S_{\widetilde{n}}\left( f \right)$")
+plt.xlim(-50, 2000)
+plt.savefig("forcing_spectra_4e5.eps", bbox_inches="tight")
+plt.show()
+
