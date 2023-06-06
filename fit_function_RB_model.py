a0171c43b5f23c4bd59eb64f8403d9c25184aa26
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-06-06 14:03:22 +0200

    start with constrained fit

--- fit_function_RB_model.py
+++ fit_function_RB_model.py
@@ -56,3 +56,35 @@ def create_fit_RB(regime, f, dt, PSD, normalized_data, T):
         res.x, normalized_data, time_kern, dt, duration_time, T
     )
     return time_series_fit, symbols, duration_time, forcing
+
+def constrained_fit_RB(regime, f, dt, PSD, normalized_data, T):
+    """calculates fit for Lorenz system time series"""
+    symbols = ""
+
+    fitrange = {"4e5": (f < 1600) & (f > 700), "2e6": (f < 5200) & (f > 3500)}
+    duration_time = calculate_duration_time(
+        f[fitrange[regime]], PSD[(fitrange[regime])]
+    )
+
+    kernrad = 2**18
+    time_kern = np.arange(-kernrad, kernrad + 1) * dt
+
+    def obj_fun(x):
+        return 0.5 * np.sum(
+            (
+                generate_fpp(x, normalized_data, time_kern, dt, duration_time, T)[0]
+                ** 2
+                - normalized_data**2
+            )
+            ** 2
+        )
+
+    res = minimize(
+        obj_fun,
+        [1.0, 1.0, 0.0, 0.0],
+        bounds=((0.0, 2.0), (0.0, 2.0), (-0.99, 0.99), (-0.5, 0.5)),
+    )
+    time_series_fit, forcing = generate_fpp(
+        res.x, normalized_data, time_kern, dt, duration_time, T
+    )
+    return time_series_fit, symbols, duration_time, forcing
