fa22e0880325286e84eb110d99cdd94fc090ffa3
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-06-08 11:48:43 +0200

    added fixed amp option

--- fit_function_RB_model.py
+++ fit_function_RB_model.py
@@ -24,6 +24,20 @@ def generate_fpp(var, normalized_data, tkern, dt, td, T):
     time_series_fit = (time_series_fit - time_series_fit.mean()) / time_series_fit.std()
     return time_series_fit, forcing
 
+def generate_fpp_fixed_amp(var, normalized_data, tkern, dt, td, T):
+    """generated normalized filtered point process as a fit for given data"""
+    pos_peak_loc = find_peaks(normalized_data, height=0.0001)[0]
+    neg_peak_loc = find_peaks(-normalized_data, height=0.0001)[0]
+    lam, offset = var
+    forcing = np.zeros(T.size)
+    forcing[pos_peak_loc] = normalized_data[pos_peak_loc] * 1
+    forcing[neg_peak_loc] = normalized_data[neg_peak_loc] * 1
+
+    kern = skewed_lorentz(tkern, dt, lam, td, m=offset)
+
+    time_series_fit = fftconvolve(forcing, kern, "same")
+    time_series_fit = (time_series_fit - time_series_fit.mean()) / time_series_fit.std()
+    return time_series_fit, forcing
 
 def generate_fpp_dipole(var, normalized_data, tkern, dt, td, T):
     """generated normalized filtered point process as a fit for given data"""
@@ -52,6 +66,38 @@ def return_peaks(data, T):
     peaks = sorted(np.append(pos_peak_loc, neg_peak_loc))
     return T[peaks], data[peaks], peaks
 
+
+def create_fit_RB_fixed_amp(regime, f, dt, PSD, normalized_data, T):
+    """calculates fit for Lorenz system time series"""
+    symbols = ""
+
+    fitrange = {"4e5": (f < 1600) & (f > 700), "2e6": (f < 5200) & (f > 3500)}
+    duration_time = calculate_duration_time(
+        f[fitrange[regime]], PSD[(fitrange[regime])]
+    )
+
+    kernrad = 2**18
+    time_kern = np.arange(-kernrad, kernrad + 1) * dt
+
+    def obj_fun(x):
+        return 0.5 * np.sum(
+            (
+                generate_fpp_fixed_amp(x, normalized_data, time_kern, dt, duration_time, T)[0]
+                ** 2
+                - normalized_data**2
+            )
+            ** 2
+        )
+
+    res = minimize(
+        obj_fun,
+        [0.0, 0.0],
+        bounds=((-0.99, 0.99), (-0.5, 0.5)),
+    )
+    time_series_fit, forcing = generate_fpp_fixed_amp(
+        res.x, normalized_data, time_kern, dt, duration_time, T
+    )
+    return time_series_fit, symbols, duration_time, forcing
 
 def create_fit_RB(regime, f, dt, PSD, normalized_data, T):
     """calculates fit for Lorenz system time series"""
