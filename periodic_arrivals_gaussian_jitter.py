7a9a8666ec5a777c2fd893c0cd4d3f9cd2f81e15
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-06-27 14:59:46 +0200

    fit implemented, checks required

--- periodic_arrivals_gaussian_jitter.py
+++ periodic_arrivals_gaussian_jitter.py
@@ -13,18 +13,18 @@ axes_size = cosmoplots.set_rcparams_dynamo(plt.rcParams, num_cols=1, ls="thin")
 
 fig_PSD = plt.figure()
 ax1 = fig_PSD.add_axes(axes_size)
-fig_AC = plt.figure()
-ax2 = fig_AC.add_axes(axes_size)
+# fig_AC = plt.figure()
+# ax2 = fig_AC.add_axes(axes_size)
 
 
 class ForcingQuasiPeriodic(frc.ForcingGenerator):
-    def __init__(self, nu):
-        self.nu = nu
+    def __init__(self, sigma):
+        self.sigma = sigma
 
     def get_forcing(self, times: np.ndarray, gamma: float) -> frc.Forcing:
         total_pulses = int(max(times) * gamma)
         periodic_waiting_times = np.arange(1, total_pulses + 1)
-        waiting_times_jitter = np.random.normal(loc=1, scale=self.nu, size=total_pulses)
+        waiting_times_jitter = np.random.normal(loc=1, scale=self.sigma, size=total_pulses)
         # * 100 for dt correction
         arrival_times = (periodic_waiting_times + waiting_times_jitter) * 100 / gamma
 
@@ -60,20 +60,21 @@ model = pm.PointModel(gamma=0.2, total_duration=100000, dt=0.01)
 model.set_pulse_shape(ps.LorentzShortPulseGenerator(tolerance=1e-5))
 
 colors = ["tab:blue", "tab:orange", "tab:olive"]
-for i, nu in enumerate([0.0, 0.1, 0.4]):
-    model.set_custom_forcing_generator(ForcingQuasiPeriodic(nu=nu))
+for i, sigma in enumerate([0.0, 0.1, 0.4]):
+    model.set_custom_forcing_generator(ForcingQuasiPeriodic(sigma=sigma))
 
     T, S = model.make_realization()
     forcing = model.get_last_used_forcing()
     amp = forcing.amplitudes
 
-    S_norm = (S - S.mean()) / S.std()
+    # S_norm = (S - S.mean()) / S.std()
+    S_norm = S - S.mean()
 
     f, Pxx = signal.welch(x=S_norm, fs=100, nperseg=S.size / 30)
-    ax1.semilogy(f, Pxx, label=rf"$\nu = {nu}$", color=colors[i])
+    ax1.semilogy(f, Pxx, label=rf"$\sigma = {sigma}$", color=colors[i])
 
-    tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
-    ax2.plot(tb, R, label=rf"$\nu = {nu}$", color=colors[i])
+    # tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
+    # ax2.plot(tb, R, label=rf"$\sigma = {sigma}$", color=colors[i])
 
 # PSD = PSD_periodic_arrivals(2 * np.pi * f, td=1, gamma=0.2, A_rms=1, A_mean=1, dt=0.01)
 # ax1.semilogy(f, PSD, "--k", label=r"$S_{\widetilde{\Phi}}(\tau_\mathrm{d} f)$")
@@ -81,17 +82,47 @@ for i, nu in enumerate([0.0, 0.1, 0.4]):
 # R_an = autocorr_periodic_arrivals(t, 0.2, 1, 1)
 # ax2.plot(t, R_an, "--k", label=r"$R_{\widetilde{\Phi}}(t/\tau_\mathrm{d})$")
 
+def Lorentz_PSD(theta):
+    """PSD of a single Lorentz pulse with duration time td = 1"""
+    return 2 * np.pi * np.exp(-2 * np.abs(theta))
+
+
+def find_nearest(array, value):
+    """returns array of parks in PSD"""
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+
+def spectra_analytical(omega, gamma, A_rms, A_mean, sigma, dt):
+    nu = sigma * gamma
+    Omega = omega / gamma
+    I_2 = 1 / (2 * np.pi)
+    first_term = gamma * A_rms**2 * I_2 * Lorentz_PSD(omega)
+
+    tmp = np.zeros(omega.size)
+    for n in range(-1000, 1000):
+        index = 2 * np.pi * n 
+        tmp = np.where(np.abs(Omega - find_nearest(Omega, index)) > 0.001, tmp, 1)
+
+    second_term = (2 * np.pi * gamma**2 * A_mean**2 * I_2 * Lorentz_PSD(omega) * np.exp(- nu**2 * Omega**2)) * tmp
+    return 2*(first_term + second_term / dt)
+
+
+PSD = spectra_analytical(2 * np.pi * f, gamma=0.2, A_rms=1, A_mean=1, sigma=0.9, dt = 0.01)
+ax1.semilogy(f, PSD, "--k", label=r"$S_{{\Phi}}(\tau_\mathrm{d} f)$")
+
 ax1.set_xlabel(r"$\tau_\mathrm{d} f$")
 ax1.set_ylabel(r"$S_{\widetilde{\Phi}}(\tau_\mathrm{d} f)$")
 ax1.set_xlim(-0.03, 1)
 ax1.set_ylim(1e-4, 1e2)
 ax1.legend()
 
-ax2.set_xlim(0, 50)
-ax2.set_xlabel(r"$t/\tau_\mathrm{d}$")
-ax2.set_ylabel(r"$R_{\widetilde{\Phi}}(t/\tau_\mathrm{d})$")
-ax2.legend()
-cosmoplots.change_log_axis_base(ax1, "y", base=10)
+# ax2.set_xlim(0, 50)
+# ax2.set_xlabel(r"$t/\tau_\mathrm{d}$")
+# ax2.set_ylabel(r"$R_{\widetilde{\Phi}}(t/\tau_\mathrm{d})$")
+# ax2.legend()
+# cosmoplots.change_log_axis_base(ax1, "y", base=10)
 
 # fig_PSD.savefig("PSD_different_kappa.eps", bbox_inches="tight")
 # fig_AC.savefig("AC_different_kappa.eps", bbox_inches="tight")
