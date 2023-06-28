347a901334ff8860558db9ef7a35147965353c7d
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-06-28 09:45:00 +0200

    finaize figures for 1e-4

--- spectra_1e-4.py
+++ spectra_1e-4.py
@@ -3,7 +3,10 @@ from fppanalysis import cond_av
 from scipy import signal
 import matplotlib.pyplot as plt
 from fit_function_RB_model import create_fit_K, create_fit_U
+import cosmoplots
 
+
+axes_size = cosmoplots.set_rcparams_dynamo(plt.rcParams, num_cols=1, ls="thin")
 
 def double_exp(tkern, lam, td):
     kern = np.zeros(tkern.size)
@@ -23,11 +26,17 @@ time_2 = np.load("K_time_1e-4_2.npy")
 K_3 = np.load("K_1e-4_3.npy")
 time_3 = np.load("K_time_1e-4_3.npy")
 
+K_4 = np.load("K_1e-4_4.npy")
+time_4 = np.load("K_time_1e-4_4.npy")
+
 K = np.append(K, K_2)
 time = np.append(time, time_2)
 
 K = np.append(K, K_3)
 time = np.append(time, time_3)
+
+K = np.append(K, K_4)
+time = np.append(time, time_4)
 
 K = K[2000:]
 # U = U[2000:]
@@ -63,6 +72,7 @@ wait = wait[wait > 200]
 plt.hist(wait / np.mean(wait), 32, density=True)
 plt.xlabel(r"$\tau_w/\langle\tau_w\rangle$")
 plt.ylabel(r"$P(\tau_w/\langle\tau_w\rangle)$")
+plt.savefig("P(tau)_1e-4.eps", bbox_inches="tight")
 plt.show()
 
 K = (K - np.mean(K)) / np.std(K)
@@ -81,21 +91,23 @@ K_fit, symbols, _, _ = create_fit_K(
 )
 
 plt.plot(time, K)
-plt.plot(time, K_fit, "--")
+# plt.plot(time, K_tit, "--")
 plt.xlabel(r"$t$")
 plt.ylabel(r"$\widetilde{K}$")
 plt.xlim(70000, 72000)
+plt.savefig("K_1e-4.eps", bbox_inches="tight")
 plt.show()
 
 
 f, PK_fit = signal.welch(K_fit, 1 / dt, nperseg=len(K_fit) / 4)
 
 plt.semilogy(fK, PK)
-plt.semilogy(f, PK_fit, "--")
+# plt.semilogy(f, PK_fit, "--")
 plt.xlabel(r"$f$")
 plt.ylabel(r"$S_{\widetilde{K}}\left( f \right)$")
 plt.xlim(-0.003, 0.03)
 plt.ylim(1e-1, None)
+plt.savefig("S(K)_1e-4.eps", bbox_inches="tight")
 plt.show()
 
 #
