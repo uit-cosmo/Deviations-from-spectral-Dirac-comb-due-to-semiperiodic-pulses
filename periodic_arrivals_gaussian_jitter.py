8baf4b6aa9c182802713e2403561a49361599f11
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-06-29 11:54:51 +0200

    corrected first term of analytical expression

--- periodic_arrivals_gaussian_jitter.py
+++ periodic_arrivals_gaussian_jitter.py
@@ -13,13 +13,13 @@ axes_size = cosmoplots.set_rcparams_dynamo(plt.rcParams, num_cols=1, ls="thin")
 
 fig_PSD = plt.figure()
 ax1 = fig_PSD.add_axes(axes_size)
-# fig_AC = plt.figure()
-# ax2 = fig_AC.add_axes(axes_size)
+fig_AC = plt.figure()
+ax2 = fig_AC.add_axes(axes_size)
 
 
 class ForcingQuasiPeriodic(frc.ForcingGenerator):
-    def __init__(self, variance):
-        self.sigma = np.sqrt(variance)
+    def __init__(self, sigma):
+        self.sigma = sigma
 
     def get_forcing(self, times: np.ndarray, gamma: float) -> frc.Forcing:
         total_pulses = int(max(times) * gamma)
@@ -39,6 +39,10 @@ class ForcingQuasiPeriodic(frc.ForcingGenerator):
         arrival_time_indx = arrival_time_indx[arrival_time_indx < times.size]
 
         amplitudes = np.random.default_rng().exponential(scale=1.0, size=total_pulses)
+        # amplitudes = np.random.default_rng().normal(loc=0,scale=1.0, size=total_pulses)
+        # amplitudes = np.ones(total_pulses)
+        print(np.mean(amplitudes))
+        print(np.std(amplitudes))
         durations = np.ones(shape=total_pulses)
 
         return frc.Forcing(
@@ -62,21 +66,31 @@ model = pm.PointModel(gamma=0.2, total_duration=100000, dt=0.01)
 model.set_pulse_shape(ps.LorentzShortPulseGenerator(tolerance=1e-5))
 
 colors = ["tab:blue", "tab:orange", "tab:olive"]
-for i, variance in enumerate([0.01]):#, 0.01, 0.04]):
-    model.set_custom_forcing_generator(ForcingQuasiPeriodic(variance=variance))
+for i, sigma in enumerate([0.0, 0.01, 0.3]):
+    model.set_custom_forcing_generator(ForcingQuasiPeriodic(sigma=sigma))
 
     T, S = model.make_realization()
     forcing = model.get_last_used_forcing()
+    F = np.zeros(len(S))
+    matching_indexes = np.isin(T, forcing.arrival_times)
+    F[matching_indexes] = 1
+    # plt.plot(T, S)
+    # plt.plot(T, F)
+    # plt.show()
     amp = forcing.amplitudes
 
     # S_norm = (S - S.mean()) / S.std()
     S_norm = S - S.mean()
+    F_norm = F - F.mean()
 
     f, Pxx = signal.welch(x=S_norm, fs=100, nperseg=S.size / 30)
-    ax1.semilogy(f, Pxx, label=rf"$\sigma = {variance}$", color=colors[i])
+    # f, Pxx = signal.welch(x=F_norm, fs=100, nperseg=S.size / 30)
+    # f, Pxx = signal.periodogram(x=S_norm, fs=100)
+    print(f"Integrated spectrum: {np.trapz(Pxx,f)}")
+    ax1.semilogy(f, Pxx, label=rf"$\sigma = {sigma}$", color=colors[i])
 
-    # tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
-    # ax2.plot(tb, R, label=rf"$\sigma = {variance}$", color=colors[i])
+    tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
+    ax2.plot(tb, R, label=rf"$\sigma = {sigma}$", color=colors[i])
 
 # PSD = PSD_periodic_arrivals(2 * np.pi * f, td=1, gamma=0.2, A_rms=1, A_mean=1, dt=0.01)
 # ax1.semilogy(f, PSD, "--k", label=r"$S_{\widetilde{\Phi}}(\tau_\mathrm{d} f)$")
@@ -101,7 +115,7 @@ def spectra_analytical(omega, gamma, A_rms, A_mean, sigma, dt):
     nu = sigma * gamma
     Omega = omega / gamma
     I_2 = 1 / (2 * np.pi)
-    first_term = gamma * A_rms**2 * I_2 * Lorentz_PSD(omega)
+    first_term = gamma * (A_rms**2 + A_mean**2*(1 - np.exp(-(nu**2) * Omega**2))) * I_2 * Lorentz_PSD(omega)
 
     tmp = np.zeros(omega.size)
     for n in range(-1000, 1000):
@@ -110,7 +124,7 @@ def spectra_analytical(omega, gamma, A_rms, A_mean, sigma, dt):
 
     second_term = (
         2
-        * np.pi**2
+        * np.pi
         * gamma**2
         * A_mean**2
         * I_2
@@ -121,9 +135,18 @@ def spectra_analytical(omega, gamma, A_rms, A_mean, sigma, dt):
 
 gamma = 0.2
 PSD = spectra_analytical(
-    2 * np.pi * f, gamma=gamma, A_rms=1, A_mean=1, sigma=0.1/gamma, dt=0.01
+    2 * np.pi * f, gamma=gamma, A_rms=1, A_mean=1, sigma=0.0/gamma, dt=0.01
 )
 ax1.semilogy(f, PSD, "--k", label=r"$S_{{\Phi}}(\tau_\mathrm{d} f)$")
+
+PSD = spectra_analytical(
+    2 * np.pi * f, gamma=gamma, A_rms=1, A_mean=1, sigma=0.01/gamma, dt=0.01
+)
+ax1.semilogy(f, PSD, "--k")
+PSD = spectra_analytical(
+    2 * np.pi * f, gamma=gamma, A_rms=1, A_mean=1, sigma=0.3/gamma, dt=0.01
+)
+ax1.semilogy(f, PSD, "--k")
 
 # PSD = spectra_analytical(
 #     2 * np.pi * f, gamma=0.2, A_rms=1, A_mean=1, sigma=0.1, dt=0.01
@@ -132,17 +155,17 @@ ax1.semilogy(f, PSD, "--k", label=r"$S_{{\Phi}}(\tau_\mathrm{d} f)$")
 # ax1.semilogy(f, PSD, "--k", label=r"$S_{{\Phi}}(\tau_\mathrm{d} f)$")
 ax1.set_xlabel(r"$\tau_\mathrm{d} f$")
 ax1.set_ylabel(r"$S_{{\Phi}}(\tau_\mathrm{d} f)$")
-ax1.set_xlim(-0.03, 1)
+ax1.set_xlim(-0.03, 0.8)
 ax1.set_ylim(1e-4, 1e2)
 ax1.legend()
 #
-# ax2.set_xlim(0, 50)
-# ax2.set_xlabel(r"$t/\tau_\mathrm{d}$")
-# ax2.set_ylabel(r"$R_{\widetilde{\Phi}}(t/\tau_\mathrm{d})$")
-# ax2.legend()
-# cosmoplots.change_log_axis_base(ax1, "y", base=10)
+ax2.set_xlim(0, 50)
+ax2.set_xlabel(r"$t/\tau_\mathrm{d}$")
+ax2.set_ylabel(r"$R_{\widetilde{\Phi}}(t/\tau_\mathrm{d})$")
+ax2.legend()
+cosmoplots.change_log_axis_base(ax1, "y", base=10)
 
-# fig_PSD.savefig("PSD_gaussian_jitter.eps", bbox_inches="tight")
-# fig_AC.savefig("AC_gaussian_jitter.eps", bbox_inches="tight")
+fig_PSD.savefig("PSD_gaussian_jitter.eps", bbox_inches="tight")
+fig_AC.savefig("AC_gaussian_jitter.eps", bbox_inches="tight")
 
 plt.show()
