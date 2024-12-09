cdf34964de450aabcea6069a63b38bd5c181c5ff
Audun Theodorsen <audun.theodorsen@uitn.no>
2024-12-09 11:14:58 +0100

    fix asymmetric laplace

--- create_figure_asym_lap_amp.py
+++ create_figure_asym_lap_amp.py
@@ -1,10 +1,14 @@
 """
-Power spectral density and autocorrelation function of asymetrically laplace distributed amplitudes with different asymmetry parameters.
+Power spectral density and autocorrelation function
+of asymetrically laplace distributed amplitudes with different asymmetry parameters.
+
+Creates fig. 4 in the most recent manuscript.
 """
 
 import numpy as np
 from scipy import signal
-from support_functions import *
+import fppanalysis as fa
+import support_functions as sf
 import superposedpulses.forcing as frc
 import superposedpulses.point_model as pm
 import superposedpulses.pulse_shape as ps
@@ -12,9 +16,10 @@ from scipy.signal import find_peaks
 
 import matplotlib.pyplot as plt
 import cosmoplots
+
 plt.style.use("cosmoplots.default")
 
-fig, ax = cosmoplots.figure_multiple_rows_columns(1,2)
+fig, ax = cosmoplots.figure_multiple_rows_columns(1, 2)
 cosmoplots.change_log_axis_base(ax[0], "y")
 
 
@@ -27,7 +32,7 @@ class AsymLaplaceAmp(frc.ForcingGenerator):
         arrival_time_indx = (
             np.arange(start=0, stop=99994, step=5) * 100
         )  # multiplied with inverse dt
-        amplitudes = sample_asymm_laplace(
+        amplitudes = sf.sample_asymm_laplace(
             alpha=0.5
             / np.sqrt(
                 1.0 - 2.0 * self.control_parameter * (1.0 - self.control_parameter)
@@ -50,7 +55,7 @@ class AsymLaplaceAmp(frc.ForcingGenerator):
         pass
 
 
-model = pm.PointModel(gamma=0.2, total_duration=100000, dt=0.01)
+model = pm.PointModel(gamma=0.2, total_duration=100_000, dt=0.01)
 model.set_pulse_shape(ps.LorentzShortPulseGenerator(tolerance=1e-5))
 
 for color, control_parameter in enumerate([0.2, 0.4, 0.45, 0.48]):
@@ -64,12 +69,14 @@ for color, control_parameter in enumerate([0.2, 0.4, 0.45, 0.48]):
     S_norm = (S - S.mean()) / S.std()
 
     f, Pxx = signal.welch(x=S_norm, fs=100, nperseg=S.size / 30)
-    ax[0].plot(f, Pxx, label=rf"$\lambda = {control_parameter}$", c='C{}'.format(color))
+    ax[0].plot(f, Pxx, label=rf"$\lambda = {control_parameter}$", c="C{}".format(color))
 
-    fitrange = find_peaks(Pxx[(f < 1)], distance=500, height=[5e-4, 1e3])[0]
-    ax[0].plot(f[fitrange][1:], Pxx[fitrange][1:], "o", c='C{}'.format(color))
+    height = 20 * np.exp(-2 * 2 * np.pi * f)
 
-    tb, R = corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
+    fitrange = find_peaks(Pxx[(f < 1)], distance=10, height=height[f < 1])[0]
+    ax[0].plot(f[fitrange], Pxx[fitrange], "o", c="C{}".format(color))
+
+    tb, R = fa.corr_fun(S_norm, S_norm, dt=0.01, norm=False, biased=True, method="auto")
     ax[1].plot(tb, R, label=rf"$\lambda = {control_parameter}$")
 
 ax[0].legend()
